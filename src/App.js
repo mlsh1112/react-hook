627faf00da8d04a164324810836df49b7f07e943
LeeSeohyun <59257758+mlsh1112@users.noreply.github.com>
2021-04-28 02:31:11 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import "./styles.css";
 import React, { useState } from "react";
 
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    //const value=event.target.value;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
 export default function App() {
+  const maxlen = (value) => value.length < 10;
+  const name = useInput("Mr.", maxlen);
   return (
     <div className="App">
       <h1>Hello </h1>
+      <input placeholder="Name" value={name.value} onChange={name.onChange} />
+      <input placeholder="Name" {...name} />
     </div>
   );
 }
