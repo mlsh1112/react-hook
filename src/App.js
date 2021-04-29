e54d81042545b4f6d523e702a68314dcee1ccd16
LeeSeohyun <59257758+mlsh1112@users.noreply.github.com>
2021-04-29 23:50:15 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./styles.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
 
 export default function App() {
+  const titleUpdater = useTitle("Loading ...");
+  setTimeout(() => titleUpdater("Home"), 5000);
   return (
     <div className="App">
       <h1>Hello </h1>
